ed4d93337689d5e0dee3379a2f39741fb69f7600
Moustafa Magdy <125929204+MoustafaMagdy10@users.noreply.github.com>
2024-11-17 20:55:00 +0200

    implement sbrk

--- kern/mem/kheap.c
+++ kern/mem/kheap.c
@@ -39,27 +39,76 @@ int initialize_kheap_dynamic_allocator(uint32 daStart, uint32 initSizeToAllocate
 	return 0;
 }
 
-void* sbrk(int numOfPages)
+void *sbrk(int numOfPages)
 {
-	/* numOfPages > 0: move the segment break of the kernel to increase the size of its heap by the given numOfPages,
-	 * 				you should allocate pages and map them into the kernel virtual address space,
-	 * 				and returns the address of the previous break (i.e. the beginning of newly mapped memory).
-	 * numOfPages = 0: just return the current position of the segment break
-	 *
-	 * NOTES:
-	 * 	1) Allocating additional pages for a kernel dynamic allocator will fail if the free frames are exhausted
-	 * 		or the break exceed the limit of the dynamic allocator. If sbrk fails, return -1
-	 */
 
-	//MS2: COMMENT THIS LINE BEFORE START CODING==========
-	return (void*)-1 ;
-	//====================================================
+	// if inc equal 0 then return the curr brk
+	if (numOfPages == 0)
+	{
+		return (void *)brk;
+	}
 
-	//TODO: [PROJECT'24.MS2 - #02] [1] KERNEL HEAP - sbrk
-	// Write your code here, remove the panic and write your code
-	panic("sbrk() is not implemented yet...!!");
+	else if (numOfPages > 0)
+	{
+		
+		/*
+			b7wl al nwbrk le char 34an am4y byte byte 34an ama kont m5liha int mknt4 btzbot
+		*/
+		uint32 oldBrk = brk;
+		char* newBrk = (char*)brk ;
+
+		// bzwd 4096 fe 3dd al pages da al bt7rko wa ba round le 22rb page
+		newBrk+=(numOfPages*PAGE_SIZE) ;
+		newBrk = ROUNDUP(newBrk, PAGE_SIZE);
+		if ((int32)newBrk > rlimit)
+		{
+			return (void *)-1; 
+		}
+		
+		uint32 currAdress = brk;
+		while (currAdress < (int32)newBrk )
+		{
+			// bgib frame mn al memory a3mlo allocate wa a mapo ll app wa azwd al itterator
+			struct FrameInfo *frame;
+			allocate_frame(&frame);
+			map_frame(ptr_page_directory, frame, currAdress, PERM_WRITEABLE);
+			currAdress += PAGE_SIZE;
+		}
+
+		// al brk dlw2ty b2a hna wa brg3 al old 
+		brk = (int32)newBrk; 
+		return (void *)oldBrk;	 
+	}
+
+	else if (numOfPages < 0)
+	{
+		//! -----------------warrning mlo4 test-------------------
+		// b3ml nfs al fo2 bs bl3ks
+		char* new_brk = (char*)brk ;
+		new_brk+=(numOfPages*PAGE_SIZE) ;
+
+		if ((int)new_brk < start)
+		{
+			new_brk = (char*)start;
+		}
+
+		uint32 currAdress =(int) new_brk;
+		while (currAdress < brk)
+		{
+			// todo at2kd ml 7eta dy
+			uint32 *page_table;
+        	struct FrameInfo *frame = get_frame_info(ptr_page_directory, currAdress, &page_table);
+			free_frame(frame);
+			unmap_frame(ptr_page_directory, currAdress);
+			currAdress += PAGE_SIZE;
+		}
+
+		brk = (int)new_brk;	 
+		return (void *)brk; 
+	}
+
+	return NULL; 
 }
-
 //TODO: [PROJECT'24.MS2 - BONUS#2] [1] KERNEL HEAP - Fast Page Allocator
 
 void* kmalloc(unsigned int size)
