56c597946adc2a25475fd9d7b30bf84d9e6ebd82
Apolo151 <abdogofor55@gmail.com>
2024-11-19 09:05:53 +0200

    initial kfree

--- kern/mem/kheap.c
+++ kern/mem/kheap.c
@@ -29,9 +29,9 @@ int initialize_kheap_dynamic_allocator(uint32 daStart, uint32 initSizeToAllocate
 	while (moving_address < brk)
 	{
 		struct FrameInfo *frame;
-		allocate_frame(&frame); 
+		allocate_frame(&frame);
 		map_frame(ptr_page_directory, frame, moving_address, PERM_WRITEABLE);
-		moving_address += PAGE_SIZE; 
+		moving_address += PAGE_SIZE;
 	}
 
 	initialize_dynamic_allocator(daStart, initSizeToAllocate);
@@ -50,7 +50,7 @@ void *sbrk(int numOfPages)
 
 	else if (numOfPages > 0)
 	{
-		
+
 		/*
 			b7wl al nwbrk le char 34an am4y byte byte 34an ama kont m5liha int mknt4 btzbot
 		*/
@@ -62,9 +62,9 @@ void *sbrk(int numOfPages)
 		newBrk = ROUNDUP(newBrk, PAGE_SIZE);
 		if ((int32)newBrk > rlimit)
 		{
-			return (void *)-1; 
+			return (void *)-1;
 		}
-		
+
 		uint32 currAdress = brk;
 		while (currAdress < (int32)newBrk )
 		{
@@ -75,9 +75,9 @@ void *sbrk(int numOfPages)
 			currAdress += PAGE_SIZE;
 		}
 
-		// al brk dlw2ty b2a hna wa brg3 al old 
-		brk = (int32)newBrk; 
-		return (void *)oldBrk;	 
+		// al brk dlw2ty b2a hna wa brg3 al old
+		brk = (int32)newBrk;
+		return (void *)oldBrk;
 	}
 
 	else if (numOfPages < 0)
@@ -103,11 +103,11 @@ void *sbrk(int numOfPages)
 			currAdress += PAGE_SIZE;
 		}
 
-		brk = (int)new_brk;	 
-		return (void *)brk; 
+		brk = (int)new_brk;
+		return (void *)brk;
 	}
 
-	return NULL; 
+	return NULL;
 }
 //TODO: [PROJECT'24.MS2 - BONUS#2] [1] KERNEL HEAP - Fast Page Allocator
 
@@ -119,21 +119,22 @@ void *kmalloc(unsigned int size)
     }
     if (size <= DYN_ALLOC_MAX_BLOCK_SIZE )
     {
-     
-     if(isKHeapPlacementStrategyFIRSTFIT())
-        return alloc_block_FF(size);
-        return alloc_block_BF(size);
+		if(isKHeapPlacementStrategyFIRSTFIT())
+			return alloc_block_FF(size);
 
+		return alloc_block_BF(size);
     }
+
+    // bounds setup
     uint32 needed_pages = size / PAGE_SIZE;
-needed_pages += (size % PAGE_SIZE != 0) ? 1 : 0;
+    needed_pages += (size % PAGE_SIZE != 0) ? 1 : 0;
 
     uint32 bottom_bound = rlimit + PAGE_SIZE;
     uint32 top_bound = KERNEL_HEAP_MAX;
 
     uint32 free_pages_found = 0;
     uint32 loop_address = bottom_bound;
-
+    // loop allocating frames
     while (loop_address < top_bound)
     {
         uint32 *page_table;
@@ -166,10 +167,50 @@ void kfree(void* virtual_address)
 {
 	//TODO: [PROJECT'24.MS2 - #04] [1] KERNEL HEAP - kfree
 	// Write your code here, remove the panic and write your code
-	panic("kfree() is not implemented yet...!!");
+	//panic("kfree() is not implemented yet...!!");
 
 	//you need to get the size of the given allocation using its address
 	//refer to the project presentation and documentation for details
+	// Convert the virtual address to uint32 for easier range checks
+	uint32 va = (uint32)virtual_address;
+
+	// Case 1: Check if the address is within the [BLOCK ALLOCATOR] range
+	if (va >= KERNEL_HEAP_START && va < rlimit)
+	{
+		// Use the dynamic allocator to free the block
+		free_block(virtual_address);
+		return;
+	}
+
+	// Case 2: Check if the address is within the [PAGE ALLOCATOR] range
+	if (va >= rlimit+PAGE_SIZE && va < KERNEL_HEAP_MAX)
+	{
+		// Get the size of the allocation (number of pages) using dynamic allocator metadata
+		uint32 size = *((uint32 *)virtual_address); // This function should return the allocated size in bytes
+		if (size == 0)
+		{
+			panic("kfree() called on unallocated or invalid memory in PAGE ALLOCATOR range!");
+			return;
+		}
+		// Calculate the number of pages
+		uint32 num_pages = size / PAGE_SIZE + (size % PAGE_SIZE != 0 ? 1 : 0);
+		// Free each page in the range
+		uint32 curr_address = va;
+		for (uint32 i = 0; i < num_pages; i++)
+		{
+			struct FrameInfo* frame = get_frame_info(ptr_page_directory, curr_address, NULL);
+			if (frame != NULL)
+			{
+				free_frame(frame);              // Free the frame
+				unmap_frame(ptr_page_directory, curr_address); // Unmap the frame
+			}
+			curr_address += PAGE_SIZE;          // Move to the next page
+		}
+
+		return;
+	}
+	// Case 3: Invalid virtual address
+	panic("kfree() called with an invalid virtual address!");
 
 }
 
